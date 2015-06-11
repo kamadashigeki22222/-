04be7470d975e5e1ad2767e99789a085d431704d
kamadashigeki <pcos1fgg@s.okayama-u.ac.jp>
2015-06-11 23:15:18 +0900

    hexサイズ変更可能

--- spot_times/code_count_times.pl
+++ spot_times/code_count_times.pl
@@ -3,12 +3,23 @@ use Geo::Hex v => 3, qw(latlng2geohex geohex2latlng latlng2zone geohex2zone);
 use strict;
 use warnings;
 use File::Path;
+print "user_name ";
+   	my $user_name = <STDIN>;
+    chomp($user_name);
 
+
+    #26行目で使用Hexのサイズを変更
+print "input size=>";
+    my $level = <STDIN>;
+    chomp($level);
 
 sub count_hex{
 	my ($file)=@_;
 	my %count;
-	my $level = 10;
+   
+    
+	
+	#my $level = 10;
 	while(my $line = <$file>){
 		chomp($line);
 		my ($id,$st_time,$end_time,$lat,$lng,$time)=split(/,/,$line);
@@ -23,17 +34,12 @@ sub count_hex{
 }
 
 
-print "user_name ";
-   	my $user_name = <STDIN>;
-    chomp($user_name);
-
-
 
 my $file = IO::File->new();
 $file ->open("../kasai_program/result/stay_$user_name.csv") or die("cannot open the file");
 
 
-open(FP1, "> code/new_hex_spot_times_$user_name.txt") or die("cannot open the file");
+open(FP1, "> code/new_hex_spot_times_$user_name$level.txt") or die("cannot open the file");
 
 my %spot_hex = &count_hex($file);
 foreach my $key (sort{$spot_hex{$b} <=> $spot_hex{$a}}keys %spot_hex){
